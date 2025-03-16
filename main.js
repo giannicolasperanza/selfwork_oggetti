f6542b46fb7af87cbd2c33aa247fc90802b908df
Giannicola Speranza <speranzagiannicola@gmail.com>
2025-03-16 12:18:49 +0100

    primo esercizio

---
+++ main.js
@@ -0,0 +1,17 @@
+
+// -------------------- primo esercizio --------------------
+let persona = {
+    "nome" : "Giannicola",
+    "cognome" : "Speranza",
+    "età" : 30,
+    
+    saluta : function(){
+        console.log(` Ciao a tutti sono ${this.nome}`);
+        
+    }
+}
+
+persona.saluta();
+
+// ------------------------ fine primo esercizio ------------------
+
